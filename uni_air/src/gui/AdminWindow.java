76a8808caaaebbbb9988abe652723bfce69227fd
Asier Amigorena <asier.amigorena@opendeusto.es>
2024-10-15 17:24:38 +0200

    Crea la clase CreateBorder y la añade a la clase EmployeeWindow

--- uni_air/src/gui/AdminWindow.java
+++ uni_air/src/gui/AdminWindow.java
@@ -5,8 +5,8 @@ import javax.swing.JFrame;
 public class AdminWindow extends AbstractWindow {
 	private static final long serialVersionUID = 8885137220929508082L;
 
-	public AdminWindow(JFrame frame) {
-		super(frame);
+	public AdminWindow() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
 
--- uni_air/src/main/Main.java
+++ uni_air/src/main/Main.java
@@ -2,13 +2,15 @@ package main;
 
 import javax.swing.JFrame;
 
+import gui.EmployeeWindow;
+
 public class Main {
 	
 	JFrame ventanaActual;
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		EmployeeWindow v = new EmployeeWindow();
 	}
 
 }
