64129802dcdfdec6544ad2e12c72c1085af63b49
Asier Amigorena <asier.amigorena@opendeusto.es>
2024-10-15 18:04:59 +0200

    Creación de la clase Vuelos para probar datos y tabla de EmployeeWindow

--- uni_air/src/gui/EmployeeWindow.java
+++ uni_air/src/gui/EmployeeWindow.java
@@ -1,6 +1,12 @@
 package gui;
 
+import java.util.List;
+
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+import main.Vuelo;
 
 public class EmployeeWindow extends AbstractWindow {
 	static final long serialVersionUID = 4922652717777952972L;
@@ -13,6 +19,16 @@ public class EmployeeWindow extends AbstractWindow {
 		JPanel panel = new JPanel();
 		panel.setBorder(createBorder("Empleado"));
 		this.add(panel);
+		
+//		List<Vuelo> vuelos = List.of(
+//				new Vuelo("A123", 2.0, 45),
+//				new Vuelo("B234", 5.0, 150),
+//				new Vuelo("C345", 6.0, 89)
+//
+//		);
+		
+		JTable tabla = new JTable();
+		panel.add(tabla);
 		
 		setVisible(true);
 		}
---
+++ uni_air/src/main/Vuelo.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Vuelo {
+	// TODO: Crear clase Vuelo (Esto es para probar la tabla en EmployeeWindow)
+	
+	protected String codigo;
+	protected double tiempo;
+	protected int pasajeros;
+	
+	public Vuelo(String codigo, double tiempo, int pasajeros) {
+		this.codigo = codigo;
+		this.tiempo = tiempo;
+		this.pasajeros = pasajeros;
+	}
+}
