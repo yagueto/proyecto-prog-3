0dad783ef1783edd4042a5182ae5ce00d9480756
Santiago Freire <santiago.freire@opendeusto.es>
2024-10-21 20:20:10 +0200

    WIP ventana cliente - diseño básico panel búsqueda de vuelos

--- uni_air/src/gui/AbstractWindow.java
+++ uni_air/src/gui/AbstractWindow.java
@@ -17,18 +17,23 @@ public abstract class AbstractWindow extends JFrame{
 
 	/* 
 	 * Configura las propiedades al crear la ventana.
-	 * @param frame	El JFrame a configurar
+	 * @param frame:	El JFrame a configurar
 	 */
 	public AbstractWindow () {
 		this.setLocationRelativeTo(null);
 		this.setSize(640, 480);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// TODO: añadir título a la ventana
 		// TODO: podría ser interesante añadir un icono a la ventana
 	}
 	
+	/*
+	 * Crea un borde estándar para utilizar en el JFrame
+	 * @param	title	el título a añadir al borde
+	 * @return			el borde
+	 */
 	protected Border createBorder(String title) {
-		// Crea un borde con un título
 		Border border = BorderFactory.createTitledBorder(title);
 		return border;
 	}
--- uni_air/src/gui/UserWindow.java
+++ uni_air/src/gui/UserWindow.java
@@ -1,30 +1,93 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class UserWindow extends AbstractWindow {
 	private static final long serialVersionUID = 7345092960587394070L;
-<<<<<<< HEAD
 	
-=======
-
-
->>>>>>> branch 'main' of https://github.com/yagueto/proyecto-prog-3
+	/*
+	 * Ventana para el cliente, incluye lista de vuelos y búsqueda
+	 */
 	public UserWindow() {
 		super();
 		JTabbedPane tabPane = new JTabbedPane();
+
+		JPanel flightHistoryPanel = new FlightHistoryPanel();
+		JPanel flightSearchPanel = new FlightSearchPanel();
 		
-		JPanel panelHistorial = new JPanel();
-		tabPane.addTab("Mis Vuelos", panelHistorial);
+		tabPane.addTab("Mis Vuelos", flightHistoryPanel);
+		tabPane.addTab("Buscar vuelo", flightSearchPanel);
 		
 		JTable table = new JTable();
 		
-		panelHistorial.add(table);
+		
+		flightHistoryPanel.add(table);
+		
+		tabPane.setBorder(createBorder("Cliente"));
 		add(tabPane);
 		
 		this.setVisible(true);
 	}
+
+}
+
+/**
+ * Panel de búsqueda de vuelos
+ */
+class FlightSearchPanel extends JPanel {
+
+	/**
+	 * Random generated UID
+	 */
+	private static final long serialVersionUID = 4735413587006131088L;
 	
+	public FlightSearchPanel() {
+		this.setLayout(new BorderLayout());
+		
+		// Panel para campos de filtrado de resultados
+		JPanel filtersPanel = new JPanel();
+		filtersPanel.setBackground(new Color(255, 0, 0)); // DEBUG: añadido color para visibilidad
+		
+		JTextField origenField = new JTextField(10); // TODO: se podría añadir un placeholder?
+		JTextField destField = new JTextField(10);
+		JTextField datePicker = new JTextField(8); 	// TODO: se podría añadir un datePicker? añadir al menos lógica de comprobación
+		
+		JButton b = new JButton("Buscar"); // TODO: implementar
+		
+		filtersPanel.add(origenField);
+		filtersPanel.add(destField);
+		filtersPanel.add(datePicker);
+		filtersPanel.add(b);
+		this.add(filtersPanel, BorderLayout.NORTH);
+		
+		
+		// JTable para mostrar resultados
+		// TODO: implementar
+		JTable tabla = new JTable();
+		JScrollPane scrollPane = new JScrollPane(tabla);
+		this.add(scrollPane);
+	}
+}
+
+/**
+ * Panel para mostrar los vuelos comprados por el usuario
+ */
+class FlightHistoryPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 560276176324109821L;
+	
+	public void FlightSearchPanel() {
+		// TODO
+	}
 }
