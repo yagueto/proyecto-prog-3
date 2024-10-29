6820c2b7a9b7ffd8ec1336c45c70ce62e5bc7d9a
yagueto <santiago.freire@opendeusto.es>
2024-10-29 20:16:29 +0100

    WIP añade más campos a la ventana de compra de vuelos

--- uni_air/src/gui/BuyWindow.java
+++ uni_air/src/gui/BuyWindow.java
@@ -1,13 +1,25 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.ScrollPane;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 
 import main.Vuelo;
@@ -19,44 +31,89 @@ public class BuyWindow extends JFrame {
 	 */
 	private static final long serialVersionUID = -5205219596554060756L;
 	
-	private JLabel fullNameLabel;
-
+	private JTextField nameField;
+	private JTextField passportField;
+	private JTextField phoneField;
 	
 	public BuyWindow(Vuelo v) {
 		super();
-		this.setSize(320, 240);
+		this.setSize(640, 480);
 		this.setLocationRelativeTo(null);
 		
-		CardLayout cardLayout = new CardLayout();
+		// Panel al que se añaden los elementos
+		JPanel panel = new JPanel();
+		BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+		panel.setLayout(layout);
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Añade margen blanco alrededor de la ventana
 		
-		JPanel panel = new JPanel(cardLayout);
 		JScrollPane scrollPane = new JScrollPane(panel);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+		// Detalles del vuelo
+		JPanel flightDetailsPanel = new JPanel(); // TODO: mejorar display
+		JTextArea flightDetailsField = new JTextArea();
+		flightDetailsField.setText(v.toString()); // TODO: mejorar formato
+		flightDetailsField.setLineWrap(true);
+		flightDetailsField.setEditable(false);
+		flightDetailsPanel.add(flightDetailsField);
 		
-		JPanel userDataPanel = new JPanel();
-		fullNameLabel = new JLabel("Nombre completo:");
-		userDataPanel.add(fullNameLabel);
-		userDataPanel.add(new JTextField());
+		// Nombre completo:
+		JPanel userNamePanel = new JPanel();
+		JLabel fullNameLabel = new JLabel("Nombre completo:");
+		nameField = new JTextField(15);
+		userNamePanel.add(fullNameLabel);
+		userNamePanel.add(nameField);
 
-		// TODO
-		JPanel testPanel = new JPanel();
-		testPanel.add(new JProgressBar());
-		testPanel.add(new JTextField());
+		// Email
+		JPanel emailPanel = new JPanel();
+		JLabel emailLabel = new JLabel("Correo electrónico:");
+		JTextField emailField = new JTextField(15);
+		emailPanel.add(emailLabel);
+		emailPanel.add(emailField);
 
-		panel.add(userDataPanel);
-		panel.add(testPanel);
+		// DNI / Pasaporte:
+		JPanel passportPanel = new JPanel();
+		JRadioButton radioDni = new JRadioButton("DNI");
+		JRadioButton radioPassport = new JRadioButton("Pasaporte");
+		passportPanel.add(radioDni);
+		passportPanel.add(radioPassport);
+		passportField = new JTextField(15);
+		passportPanel.add(passportField);
+		
+		// Número de teléfono
+		JPanel phonePanel = new JPanel();
+		JLabel phoneLabel = new JLabel("Número de teléfono:");
+		phoneField = new JTextField(15);
+		phonePanel.add(phoneLabel, BorderLayout.WEST);
+		phonePanel.add(phoneLabel);
+		phonePanel.add(phoneField);
+
+		// Payment method
+		JPanel paymentPanel = new JPanel();
+		JLabel paymentLabel = new JLabel("Método de pago:");
+		JRadioButton creditCardRadio = new JRadioButton("Tarjeta de crédito");
+		JRadioButton paypalRadio = new JRadioButton("PayPal");
+		paymentPanel.add(paymentLabel);
+		paymentPanel.add(creditCardRadio);
+		paymentPanel.add(paypalRadio);
+		
+		// Botón de confirmación
+		JPanel confirmPanel = new JPanel();
+		JButton progressBar = new JButton("OK");
+		confirmPanel.add(progressBar);
+		
+		panel.add(flightDetailsPanel);
+		panel.add(userNamePanel);
+		panel.add(emailPanel);
+		panel.add(passportPanel);
+		panel.add(phonePanel);
+		panel.add(paymentPanel);
+		panel.add(confirmPanel);
+
 		add(scrollPane);
 		
 		this.setVisible(true);
 		
-		SwingUtilities.invokeLater(() -> {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("nos fuimos");
-			cardLayout.next(panel);
-		});
+		
 	}
 }
