fe2cb3958b1f2addead5fe1b32ae96feaf01ffb3
Asier Amigorena <asier.amigorena@opendeusto.es>
2024-10-30 10:08:03 +0100

    Elimina la lista vuelos de Main

--- uni_air/src/main/Main.java
+++ uni_air/src/main/Main.java
@@ -13,22 +13,11 @@ import gui.UserWindow;
 public class Main {
 
 	public static AbstractWindow w;
-	public static ArrayList<Vuelo> vuelos = new ArrayList<Vuelo>(List.of(
-			new Vuelo("IB1234", "LCG", "BIO", LocalDateTime.of(2024, 1, 1, 13, 0), LocalDateTime.of(2024, 1, 1, 13, 0), 19,
-					20, 1),
-			new Vuelo("IB1234", "LCG", "BIO", LocalDateTime.of(2024, 1, 1, 13, 0), LocalDateTime.of(2024, 1, 1, 13, 0), 19,
-					20, 1),
-			new Vuelo("IB1234", "LCG", "BIO", LocalDateTime.of(2024, 1, 1, 13, 0), LocalDateTime.of(2024, 1, 1, 13, 0), 19,
-					20, 1),
-			new Vuelo("IB1234", "LCG", "BIO", LocalDateTime.of(2024, 1, 1, 13, 0), LocalDateTime.of(2024, 1, 1, 13, 0), 19,
-					20, 1)
-
-	));
 
 	public static void main(String[] args) {
 		System.out.println("Lanzando...");
-		// SwingUtilities.invokeLater(() -> new EmployeeWindow());
-		// SwingUtilities.invokeLater(() -> new UserWindow());
+		//SwingUtilities.invokeLater(() -> new EmployeeWindow());
+		//SwingUtilities.invokeLater(() -> new UserWindow());
 	}
 
 }
