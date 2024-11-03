34bef71f51c9ba9b99f10866ac4c3b26cc9a0061
Oihan Bilbao <oihan.bilbao@opendeusto.es>
2024-11-03 11:33:47 +0100

    AdminWindow añadir vuelo1.0

--- uni_air/src/gui/AdminWindow.java
+++ uni_air/src/gui/AdminWindow.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -12,6 +15,7 @@ import javax.swing.table.TableColumn;
 import main.Main;
 import main.ModeloVuelo;
 import main.ModeloVuelo.TipoVentana;
+import main.Vuelo;
 
 public class AdminWindow extends AbstractWindow {
 	private static final long serialVersionUID = 8885137220929508082L;
@@ -20,10 +24,11 @@ public class AdminWindow extends AbstractWindow {
 		super();
 		setTitle("Admin");
 		
+		
 		JPanel panel= new JPanel();
 		panel.setBorder(createBorder("Admin"));
 		JButton btnAnadirVuelo= new JButton("Añadir vuelo");
-		ModeloVuelo vuelos= new ModeloVuelo(Main.vuelos, TipoVentana.ADMIN);
+		ModeloVuelo vuelos= new ModeloVuelo(Vuelo.getVuelos(), TipoVentana.ADMIN);
 		
 		JTable tabla = new JTable(vuelos);
 		JScrollPane scrollPane = new JScrollPane(tabla);
@@ -35,10 +40,16 @@ public class AdminWindow extends AbstractWindow {
 		this.add(scrollPane);
 		
 		btnAnadirVuelo.addActionListener((e)->{
-			String titulo=JOptionPane.showInputDialog("Introduce el codigo del nuevo vuelo");
+			int codigo=Integer.parseInt(JOptionPane.showInputDialog("Introduce el codigo del nuevo vuelo"));
 			String origen= JOptionPane.showInputDialog("Introduce el aeropuerto origen");
 			String destino= JOptionPane.showInputDialog("Introduce el aeropuerto destino");
+			LocalDateTime salida= LocalDateTime.parse(JOptionPane.showInputDialog("Introduce la fecha y hora de salida. De la siguiente forma:'AÑO-MES-DIA-Thora:minuto'"));
+			LocalDateTime llegada= LocalDateTime.parse(JOptionPane.showInputDialog("Introduce la fecha y hora de llegada. De la siguiente forma:'AÑO-MES-DIA-Thora:minuto'"));
 			
+			Vuelo vuelo= new Vuelo(codigo, origen, destino, salida, llegada, codigo, codigo, getOpacity());
+			JOptionPane.showMessageDialog(null, "Vuelo añadido correctamente");
+			
+		
 		});
 		
 		
@@ -49,12 +60,7 @@ public class AdminWindow extends AbstractWindow {
 		
 	}
 
-public static void main(String[] args) {
-	
 
-	new AdminWindow();
-	
-	}
 	
 
 }
