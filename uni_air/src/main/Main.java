4a92d924d9dff8ff5d8d1e3bc6d16c8d7280e6f4
Santiago Freire <santiago.freire@opendeusto.es>
2024-11-27 12:54:24 +0100

    Añade librería de LAF

--- uni_air/src/main/Main.java
+++ uni_air/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
 import gui.LoginWindow;
 
 import javax.swing.*;
@@ -8,6 +9,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Lanzando...");
+        try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("ERROR: No se ha encontrado la librería de Look-and-Feel FlatLAF. Utilizando el LAF " +
+                    "por defecto.");
+        }
         //SwingUtilities.invokeLater(() -> new EmployeeWindow());
         SwingUtilities.invokeLater(() -> new LoginWindow());
     }
