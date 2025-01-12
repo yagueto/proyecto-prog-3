9ccfd3bc3884965f86abed6e1d509c4ca6690755
Oihan Bilbao <oihan.bilbao@opendeusto.es>
2025-01-12 12:09:35 +0100

    Corrige merge

--- uni_air/src/gui/SignInWindow.java
+++ uni_air/src/gui/SignInWindow.java
@@ -14,114 +14,155 @@ import java.util.GregorianCalendar;
 public class SignInWindow extends AbstractWindow {
 
     private static final long serialVersionUID = 8522535521661703102L;
-    private final JTextField txtNombreUsuario;
-    private final JPasswordField txtpasswordUsuario;
-    private final JTextField txtDni;
-    private final JTextField txtApellido;
-    private final JTextField txtMail;
 
 
     public SignInWindow() {
+        JTextField txtNombreUsuario;
+        JPasswordField txtpasswordUsuario;
+        JTextField txtDni;
+        JTextField txtApellido;
+        JTextField txtMail;
+
 
         setTitle("Registro de Usuario");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setLayout(new BorderLayout(10, 10));
 
 
-        JPanel pNorte = new JPanel();
-        JPanel pSur = new JPanel();
-        JPanel pCentro = new JPanel(new GridBagLayout());
-        JPanel pOeste = new JPanel();
+        JPanel pSur = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel pCentro = new JPanel();
+        pCentro.setLayout(new BoxLayout(pCentro, BoxLayout.Y_AXIS));
+
         JPanel pEste = new JPanel();
+        JPanel pOeste = new JPanel();
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(40, 10, 10, 10); // Margen entre componentes
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        setTitle("Registro de Usuario");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout(10, 10));
 
 
-        getContentPane().add(pNorte, BorderLayout.NORTH);
-        getContentPane().add(pEste, BorderLayout.EAST);
-        getContentPane().add(pOeste, BorderLayout.WEST);
-        getContentPane().add(pSur, BorderLayout.SOUTH);
-        getContentPane().add(pCentro, BorderLayout.CENTER);
+        JLabel lblTitulo = new JLabel("¡Bienvenido!", SwingConstants.CENTER);
+        lblTitulo.setFont(new Font("Arial", Font.BOLD, 24));
+        add(lblTitulo, BorderLayout.NORTH);
+
+
+        JLabel imagenI = new JLabel(new ImageIcon("/images/Imagen_avion_2.png"));
+        JLabel imagenD = new JLabel(new ImageIcon("/images/Imagen_avion_2.png"));
+
+
+        pEste.add(imagenD);
+        pOeste.add(imagenI);
+
+
+        JLabel lblNombreUsuario = new JLabel("Introduce tu nombre: ");
+        JLabel lblApellido = new JLabel("Introduce tu apellido: ");
+        JLabel lblDni = new JLabel("Introduce tu DNI: ");
+        JLabel lblMail = new JLabel("Introduce tu MAIL: ");
+        JLabel lblPassword = new JLabel("Introduce tu contraseña: ");
+        JLabel lblBirthDate = new JLabel("Introduce tu fecha de nacimiento: ");
+
+        txtNombreUsuario = new JTextField(15);
+        txtApellido = new JTextField(15);
+        txtDni = new JTextField(15);
+        txtMail = new JTextField(15);
+        txtpasswordUsuario = new JPasswordField(15);
+        JDatePicker datePicker = new JDatePicker();
+
+        pCentro.add(lblNombreUsuario);
+        pCentro.add(txtNombreUsuario);
+        pCentro.add(Box.createRigidArea(new Dimension(25, 25)));
+        pCentro.add(lblApellido);
+        pCentro.add(txtApellido);
+        pCentro.add(Box.createRigidArea(new Dimension(25, 25)));
+        pCentro.add(lblDni);
+        pCentro.add(txtDni);
+        pCentro.add(Box.createRigidArea(new Dimension(25, 25)));
+        pCentro.add(lblMail);
+        pCentro.add(txtMail);
+        pCentro.add(Box.createRigidArea(new Dimension(25, 25)));
+        pCentro.add(lblPassword);
+        pCentro.add(txtpasswordUsuario);
+        pCentro.add(Box.createRigidArea(new Dimension(25, 25)));
+        pCentro.add(lblBirthDate);
+        pCentro.add(datePicker);
+
+
+        pCentro.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblNombreUsuario.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtNombreUsuario.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblApellido.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtApellido.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblDni.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtDni.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblMail.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtMail.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblPassword.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtpasswordUsuario.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblBirthDate.setAlignmentX(Component.CENTER_ALIGNMENT);
+        datePicker.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
+        add(pCentro, BorderLayout.CENTER);
+
+
+        // Panel izquierdo (OESTE) con imagen
+        JLabel leftImage = new JLabel(new ImageIcon("/images/Imagen_avion_sobre_mar.png"));
+
+        JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        leftPanel.add(leftImage);
+        add(leftPanel, BorderLayout.WEST);
+
+        // Panel derecho (ESTE) con imagen
+        JLabel rightImage = new JLabel(new ImageIcon("/images/Imagen_avion_sobre_mar.png"));
+        JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        rightPanel.add(rightImage);
+        add(rightPanel, BorderLayout.EAST);
+
 
         JButton btnRegistrarse = new JButton("REGISTRARME");
         JButton btnVolver = new JButton("VOLVER");
-        getRootPane().setDefaultButton(btnRegistrarse);
-
-        JLabel lblTitulo = new JLabel("¡Bienvenido!");
-        JLabel lblNombreUsuario = new JLabel("Introduce tu nombre: ");
-        JLabel lblMail = new JLabel("Introduce tu MAIL:");
-        JLabel lblpasswordUsuario = new JLabel("| Introduce tu contraseña: ");
-        JLabel lblDni = new JLabel("Introduce tu DNI: ");
-        JLabel lblApellido = new JLabel("Introduce tu Apellido: ");
-        JLabel lblbirthDate = new JLabel("Introduce tu fecha de nacimiento: ");
-
-        JDatePicker datePicker = new JDatePicker();
-
-
-        txtNombreUsuario = new JTextField(10);
-        txtpasswordUsuario = new JPasswordField(10);
-        txtDni = new JTextField(10);
-        txtApellido = new JTextField(10);
-        txtMail = new JTextField(10);
-
-
         pSur.add(btnRegistrarse);
         pSur.add(btnVolver);
-
-        pNorte.add(lblTitulo);
-
-
-        // Primera fila: Nombre
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 1;
+        add(pSur, BorderLayout.SOUTH);
+     /* Primera fila: Nombre
+        gbc.gridx = 0; gbc.gridy = 1; gbc.gridwidth = 1;
         pCentro.add(lblNombreUsuario, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridx = 1; gbc.gridy = 1;
         pCentro.add(txtNombreUsuario, gbc);
 
         // Segunda fila: Apellido
-        gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridx = 0; gbc.gridy = 2;
         pCentro.add(lblApellido, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridx = 1; gbc.gridy = 2;
         pCentro.add(txtApellido, gbc);
 
         // Tercera fila: DNI
-        gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridx = 0; gbc.gridy = 3;
         pCentro.add(lblDni, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 3;
+        gbc.gridx = 1; gbc.gridy = 3;
         pCentro.add(txtDni, gbc);
 
         // Cuarta fila: Mail
-        gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridx = 0; gbc.gridy = 4;
         pCentro.add(lblMail, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 4;
+        gbc.gridx = 1; gbc.gridy = 4;
         pCentro.add(txtMail, gbc);
 
-
-        gbc.gridx = 0;
-        gbc.gridy = 5;
+        
+        gbc.gridx = 0; gbc.gridy = 5;
         pCentro.add(lblpasswordUsuario, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 5;
+        gbc.gridx = 1; gbc.gridy = 5;
         pCentro.add(txtpasswordUsuario, gbc);
-
-
-        gbc.gridx = 0;
-        gbc.gridy = 6;
+        
+       
+        gbc.gridx = 0; gbc.gridy = 6;
         pCentro.add(lblbirthDate, gbc);
-        gbc.gridx = 1;
-        gbc.gridy = 6;
+        gbc.gridx = 1; gbc.gridy = 6;
         pCentro.add(datePicker, gbc);
 
+        */
 
         btnVolver.addActionListener(e -> {
             new LoginWindow();
